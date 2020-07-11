5b73472c56d7b4a2c5a3acaf57c8de6026c7de36
debashis1992 <d2011das@gmail.com>
2020-07-11 17:10:12 +0530

    adding bst basic

---
+++ programming-practice/src/bst/BinarySearchTreeTest.java
@@ -0,0 +1,67 @@
+package bst;
+
+public class BinarySearchTreeTest {
+
+    public static void main(String[] args) {
+        int[] a = {-10, -3, 0, 5 ,9};
+        BST bst = new BST();
+        for(int i: a)
+            bst.addNode(i);
+
+        bst.printInOrderTraversal();
+    }
+}
+
+
+class Node {
+    int value;
+    Node leftNode;
+    Node rightNode;
+
+    public Node(int value) {
+        this.value = value;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+}
+class BST {
+    Node rootNode;
+    public void addNode(int value) {
+        if(rootNode==null) {
+            Node node = new Node(value);
+            rootNode = node;
+        } else {
+            boolean isLess = false;
+            Node currNode = rootNode;
+            Node prev = null;
+            while(currNode!=null) {
+                prev = currNode;
+                if(value < currNode.value) {
+                    currNode = currNode.leftNode;
+                    isLess = true;
+                } else {
+                    currNode = currNode.rightNode;
+                    isLess = false;
+                }
+            }
+            currNode = new Node(value);
+            if(isLess)
+                prev.leftNode = currNode;
+            else prev.rightNode = currNode;
+
+        }
+    }
+
+    public void printInOrderTraversal() {
+        Node currNode = rootNode;
+        printInOrderTraversal(currNode);
+    }
+    private void printInOrderTraversal(Node node) {
+        if(node==null)
+            return;
+        printInOrderTraversal(node.leftNode);
+        System.out.println(node.value);
+        printInOrderTraversal(node.rightNode);
+    }
+
+}
