92bc1db5d9a7f7d170b3b3d42507fb2e1dc6e6f1
Debashis <debashis@Debashiss-MacBook-Pro.local>
2021-09-13 11:38:06 +0530

    adding percolation solution

--- programming-practice/src/coursera/algo1/Percolation.java
+++ programming-practice/src/coursera/algo1/Percolation.java
@@ -2,18 +2,29 @@ package coursera.algo1;
 
 import edu.princeton.cs.algs4.*;
 
+import java.util.Arrays;
+
 public class Percolation {
     // creates n-by-n grid, with all sites initially blocked
-    int[] cells;
-    int gridLength;
-    WeightedQuickUnionUF weightedQuickUnionUF;
+    private int[] cells;
+    private boolean[] isSiteOpen;
+    private int gridLength;
+    private WeightedQuickUnionUF sites;
+    private int numberOfOpenSites;
+    private int virtualTopID;
+    private int virtualDownID;
 
     public Percolation(int n) {
         if(n<=0)
             throw new IllegalArgumentException("size should be between 1 and n.");
-        cells = new int[n*n];
-        weightedQuickUnionUF = new WeightedQuickUnionUF(cells.length);
+        cells = new int[n*n+2];
+        sites = new WeightedQuickUnionUF(cells.length);
         gridLength = n;
+        numberOfOpenSites = 0;
+        virtualTopID = n*n;
+        virtualDownID = n+n+1;
+        this.isSiteOpen = new boolean[n*n];
+        Arrays.fill(isSiteOpen,false);
     }
 
     // opens the site (row, col) if it is not open already
@@ -27,77 +38,59 @@ public class Percolation {
 
         int currentPosition = find2Dto1DPosition(row, col);
         if(row-2>=0) {
+            if (row-2==0) //if topmost row is found, link it with virtualTopID
+                sites.union(currentPosition, virtualTopID);
             int topPosition = find2Dto1DPosition(row-1, col);
-            weightedQuickUnionUF.union(currentPosition, topPosition);
+            sites.union(currentPosition, topPosition);
         }
         if(col-2>=0) {
             int leftPosition = find2Dto1DPosition(row, col-1);
-            weightedQuickUnionUF.union(currentPosition, leftPosition);
+            sites.union(currentPosition, leftPosition);
+            isSiteOpen[leftPosition]=true;
         }
         if(row<gridLength) {
+            if(row==gridLength-1) //if down-most row is found, link it with virtualDownID
+                sites.union(currentPosition, virtualDownID);
             int downPosition = find2Dto1DPosition(row+1, col);
-            weightedQuickUnionUF.union(currentPosition, downPosition);
+            sites.union(currentPosition, downPosition);
+            isSiteOpen[downPosition]=true;
         }
         if(col<gridLength) {
             int rightPosition = find2Dto1DPosition(row, col+1);
-            weightedQuickUnionUF.union(currentPosition, rightPosition);
+            sites.union(currentPosition, rightPosition);
+            isSiteOpen[rightPosition]=true;
         }
+        isSiteOpen[currentPosition]=true;
+        ++numberOfOpenSites;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
 
         int currentPosition = find2Dto1DPosition(row, col);
-        if(row-2>=0) {
-            int topPosition = find2Dto1DPosition(row-1, col);
-            return weightedQuickUnionUF.find(currentPosition) == weightedQuickUnionUF.find(topPosition);
-        }
-        if(col-2>=0) {
-            int leftPosition = find2Dto1DPosition(row, col-1);
-            return weightedQuickUnionUF.find(currentPosition) == weightedQuickUnionUF.find(leftPosition);
-        }
-        if(row<gridLength) {
-            int downPosition = find2Dto1DPosition(row+1, col);
-            return weightedQuickUnionUF.find(currentPosition) == weightedQuickUnionUF.find(downPosition);
-        }
-        if(col<gridLength) {
-            int rightPosition = find2Dto1DPosition(row, col+1);
-            return weightedQuickUnionUF.find(currentPosition) == weightedQuickUnionUF.find(rightPosition);
-        }
-        return false;
+        return this.isSiteOpen[currentPosition];
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col) { return true; }
+    public boolean isFull(int row, int col) {
+        int currentPosition = find2Dto1DPosition(row, col);
+        return this.sites.find(currentPosition) == this.sites.find(this.virtualTopID);
+    }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        int maxPosition = find2Dto1DPosition(gridLength, gridLength);
-        int count=0;
-        for(int i=0;i<maxPosition;i++) {
-            if(weightedQuickUnionUF.find(i) == i)
-                ++count;
-        }
-        return count;
+        return this.numberOfOpenSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        int topMaxPosition = find2Dto1DPosition(0,0);
-        int downMaxPosition = find2Dto1DPosition(gridLength, gridLength);
-
-        for(int i=0;i<=topMaxPosition;i++) {
-            for(int j=downMaxPosition-gridLength+1;j<=downMaxPosition;j++) {
-                if(weightedQuickUnionUF.find(i) == weightedQuickUnionUF.find(j))
-                    return true;
-            }
-        }
-        return false;
+        //check if the virtual top ID is connected to virtual down ID
+        return sites.find(virtualTopID) == sites.find(virtualDownID);
     }
 
-    public int find2Dto1DPosition(int row, int col) {
+    private int find2Dto1DPosition(int row, int col) {
         if(row==0 && col==0) return 0;
-        return (row-1) * gridLength + col - 1;
+        return (row-1) * gridLength + (col-1);
     }
 
     // test client (optional)
@@ -112,9 +105,11 @@ public class Percolation {
         StdOut.println("percolation : "+percolation.percolates());
         StdOut.println("no. of opened sites : "+percolation.numberOfOpenSites());
 
-        percolation.open(2,2);
+//        percolation.open(2,2);
         StdOut.println("percolation : "+percolation.percolates());
         StdOut.println("no. of opened sites : "+percolation.numberOfOpenSites());
+
+        System.out.println("is site open : "+percolation.isOpen(2,2));
         StdOut.println("elapsed time: "+stopwatch.elapsedTime());
     }
 }
