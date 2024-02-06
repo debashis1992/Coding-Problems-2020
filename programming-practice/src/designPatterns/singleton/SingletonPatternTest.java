f72ab587f3b4d1f173fd5d87ecf2e3feabe4677c
debashis1992 <d2011das@gmail.com>
2024-02-06 15:30:38 +0530

    adding code

--- programming-practice/src/designPatterns/singleton/SingletonPatternTest.java
+++ programming-practice/src/designPatterns/singleton/SingletonPatternTest.java
@@ -3,16 +3,41 @@ package designPatterns.singleton;
 public class SingletonPatternTest {
 }
 
-class A {
-    private static volatile A instance;
-    private A() {}
+class ThreadUnsafeSingleton {
+    private static ThreadUnsafeSingleton instance;
+    private ThreadUnsafeSingleton() {}
 
-    public static A getInstance() {
+    public static ThreadUnsafeSingleton getInstance() {
         if(instance == null) {
-            synchronized (A.class) {
-                instance = new A();
+            instance = new ThreadUnsafeSingleton();
+        }
+        return instance;
+    }
+}
+
+class ThreadSafeSingleton {
+    private static volatile ThreadSafeSingleton instance;
+    private ThreadSafeSingleton() {}
+
+    public static ThreadSafeSingleton getInstance() {
+        if(instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                instance = new ThreadSafeSingleton();
             }
         }
         return instance;
+    }
+}
+
+class ThreadSafeSingletonWithoutSynchronized {
+    private static ThreadSafeSingletonWithoutSynchronized instance;
+    private ThreadSafeSingletonWithoutSynchronized() {}
+
+    private static class ThreadSafeInstanceHolder {
+        public static ThreadSafeSingletonWithoutSynchronized intance = new ThreadSafeSingletonWithoutSynchronized();
+    }
+
+    public static ThreadSafeSingletonWithoutSynchronized getInstance() {
+        return ThreadSafeInstanceHolder.intance;
     }
 }
