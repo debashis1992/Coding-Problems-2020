4308248c4fd6bf6480dfb75921ab277692215e42
debashis1992 <d2011das@gmail.com>
2024-02-13 20:44:24 +0530

    adding code

--- programming-practice/src/driveronbaordingmodule/MainApp.java
+++ programming-practice/src/driveronbaordingmodule/MainApp.java
@@ -1,35 +1,41 @@
 package driveronbaordingmodule;
 
 import driveronbaordingmodule.controller.DriverController;
+import driveronbaordingmodule.exception.DriverStateFailureException;
 import driveronbaordingmodule.model.Driver;
 import driveronbaordingmodule.model.OnboardingApplication;
-import driveronbaordingmodule.service.DocumentsCollectionState;
-import driveronbaordingmodule.service.DriverState;
+import driveronbaordingmodule.service.*;
 
 import java.util.Map;
 
 public class MainApp {
 
     public static DriverController driverController = new DriverController();
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws DriverStateFailureException {
+
+        // driver adding profile info from collection form
+        Map<String, String> attributes = Map.of(
+                "firstName","debashis",
+                "lastName", "das",
+                "phone", "342342342",
+                "email", "d2011das@gmail.com"
+        );
 
         Driver driver = signUp();
 
-        // driver adding profile info
-        Map<String, String> attributes = Map.of("firstName","debashis",
-                "lastName", "das", "phone", "342342342", "email", "d2011das@gmail.com");
-
-        addProfileInfo(driver, Map.of("", ""));
+        addProfileInfo(driver, attributes);
 
         // driver documents collection
         startDocumentsCollection(driver);
 
         // driver background verification
+        performDocumentsVerificationProcess(driver);
 
         // driver shipping of tracking device
+        shipTrackingDeviceToDriver(driver);
 
         // driver ready to ride
-
+        startReadyToRideState(driver);
     }
 
     public static Driver signUp() {
@@ -45,18 +51,33 @@ public class MainApp {
         return driver;
     }
 
-    public static void addProfileInfo(Driver driver, Map<String, String> attributes) throws Exception {
+    public static void addProfileInfo(Driver driver, Map<String, String> attributes) throws DriverStateFailureException {
         DriverState state = driver.getDriverState();
         state.processApplication(driver);
         state.updateDriverApplication(driver, attributes);
     }
 
-    public static Driver startDocumentsCollection(Driver driver) throws Exception {
+    public static Driver startDocumentsCollection(Driver driver) throws DriverStateFailureException {
         DriverState state = driver.setAndGetDriverState(new DocumentsCollectionState());
         state.processApplication(driver);
-
         return driver;
     }
 
+    public static Driver performDocumentsVerificationProcess(Driver driver) throws DriverStateFailureException {
+        DriverState state = driver.setAndGetDriverState(new BackgroundVerificationState());
+        state.processApplication(driver);
+        return driver;
+    }
 
+    public static Driver shipTrackingDeviceToDriver(Driver driver) throws DriverStateFailureException {
+        DriverState state = driver.setAndGetDriverState(new ShipTrackingDeviceState());
+        state.processApplication(driver);
+        return driver;
+    }
+
+    public static Driver startReadyToRideState(Driver driver) throws DriverStateFailureException {
+        DriverState state = driver.setAndGetDriverState(new ReadyToRideState());
+        state.processApplication(driver);
+        return driver;
+    }
 }
