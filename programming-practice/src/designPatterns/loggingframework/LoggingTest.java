5ad196ba357107a8b3855a9f958e3bbbda49d340
debashis1992 <d2011das@gmail.com>
2025-07-26 16:11:09 +0530

    daily commit

---
+++ programming-practice/src/designPatterns/loggingframework/LoggingTest.java
@@ -0,0 +1,208 @@
+package designPatterns.loggingframework;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class LoggingTest {
+
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger();
+        logger.info(new LogMessage("info message"));
+        logger.debug(new LogMessage("debug message"));
+        logger.warn(new LogMessage("warn message"));
+        logger.error(new LogMessage("error message"));
+
+    }
+}
+
+interface LoggingHandler extends LoggingAppender {
+    void handle(LogMessage logMessage) throws HandlerNotFoundException;
+    void forward(LogMessage logMessage) throws HandlerNotFoundException;
+    void setNextLoggingHandler(LoggingHandler loggingHandler);
+}
+
+interface Appender {
+    void append(String message);
+}
+
+interface LoggingAppender {
+    void add(Appender appender);
+    void remove(Appender appender);
+}
+
+class FileAppender implements Appender {
+    public void append(String message) {
+        System.out.println("appending to file...["+message+"]");
+    }
+}
+
+class StdAppender implements Appender {
+    public void append(String message) {
+        System.out.println("appending to standard output...["+message+"]");
+    }
+}
+
+class LoggingHandlerImpl implements LoggingHandler, LoggingAppender {
+    private final LogLevel logLevel;
+    private LoggingHandler nextLoggingHandler;
+    private final List<Appender> appenderList;
+
+    public LoggingHandlerImpl(LogLevel level) {
+        this.logLevel = level;
+        this.appenderList = new ArrayList<>();
+        appenderList.add(new FileAppender());
+        appenderList.add(new StdAppender());
+    }
+
+    public void setNextLoggingHandler(LoggingHandler nextLoggingHandler) {
+        this.nextLoggingHandler = nextLoggingHandler;
+    }
+
+    @Override
+    public void handle(LogMessage logMessage) throws HandlerNotFoundException {
+        if(logMessage.getLevel().equals(logLevel)) {
+//            System.out.println(new Date() + " __ [" + logLevel + "] __ [" + logMessage.getMessage() + "]");
+            appenderList.forEach(appender -> appender.append(logMessage.getMessage()));
+        }
+        else forward(logMessage);
+    }
+
+    @Override
+    public void forward(LogMessage logMessage) throws HandlerNotFoundException {
+        if(nextLoggingHandler!=null) {
+            nextLoggingHandler.handle(logMessage);
+        }
+        else throw new HandlerNotFoundException("next handler not found!");
+    }
+
+
+    @Override
+    public void add(Appender appender) {
+        appenderList.add(appender);
+    }
+
+    @Override
+    public void remove(Appender appender) {
+        appenderList.remove(appender);
+    }
+}
+
+
+class LogLevelConfigurationFactory {
+    public static LoggingHandler createLoggingLevelSequence() {
+        LoggingHandler info = LoggingHandlerFactory.createLoggingHandler(LogLevel.INFO);
+        LoggingHandler debug = LoggingHandlerFactory.createLoggingHandler(LogLevel.DEBUG);
+        LoggingHandler warn = LoggingHandlerFactory.createLoggingHandler(LogLevel.WARN);
+        LoggingHandler error = LoggingHandlerFactory.createLoggingHandler(LogLevel.ERROR);
+
+        //sequence goes here...
+        info.setNextLoggingHandler(debug);
+        debug.setNextLoggingHandler(warn);
+        warn.setNextLoggingHandler(error);
+
+        return info;
+    }
+}
+
+class LoggingHandlerFactory {
+
+    public static LoggingHandler createLoggingHandler(LogLevel logLevel) {
+        LoggingHandler loggingHandler = null;
+        if(logLevel.equals(LogLevel.DEBUG))
+            loggingHandler = new LoggingHandlerImpl(LogLevel.DEBUG);
+        else if(logLevel.equals(LogLevel.INFO))
+            loggingHandler = new LoggingHandlerImpl(LogLevel.INFO);
+        else if(logLevel.equals(LogLevel.WARN))
+            loggingHandler = new LoggingHandlerImpl(LogLevel.WARN);
+        else if(logLevel.equals(LogLevel.ERROR))
+            loggingHandler = new LoggingHandlerImpl(LogLevel.ERROR);
+
+        return loggingHandler;
+    }
+}
+
+
+final class Logger {
+    private static LoggingHandler loggingHandler;
+
+    private Logger() {}
+    private static volatile Logger loggerInstance;
+
+    public static Logger getLogger() {
+        if(loggerInstance == null) {
+            synchronized (Logger.class) {
+                loggerInstance = new Logger();
+                loggingHandler = LogLevelConfigurationFactory.createLoggingLevelSequence();
+            }
+        }
+        return loggerInstance;
+    }
+
+    public void log(LogLevel level, LogMessage message) {
+        try {
+            message.setLevel(level);
+            loggingHandler.handle(message);
+        } catch (HandlerNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void info(LogMessage logMessage) {
+        log(LogLevel.INFO, logMessage);
+    }
+
+    public void debug(LogMessage logMessage) {
+        log(LogLevel.DEBUG, logMessage);
+    }
+
+    public void warn(LogMessage logMessage) {
+        log(LogLevel.WARN, logMessage);
+    }
+
+    public void error(LogMessage logMessage) {
+        log(LogLevel.ERROR, logMessage);
+    }
+}
+
+enum LogLevel {
+    INFO(1), DEBUG(2), WARN(3), ERROR(4);
+
+    private final int level;
+    LogLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
+
+class LogMessage {
+    private LogLevel level;
+    private final String message;
+
+    public LogMessage(String message) {
+        this.message=message;
+    }
+
+    public LogLevel getLevel() {
+        return level;
+    }
+
+
+    public void setLevel(LogLevel level) {
+        this.level = level;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+class HandlerNotFoundException extends Exception {
+    public HandlerNotFoundException(String message) {
+        super(message);
+    }
+}
--- programming-practice/src/practice/PracticeTest.java
+++ programming-practice/src/practice/PracticeTest.java
@@ -1,19 +1,75 @@
 package practice;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeTest {
+    static List<List<String>> list;
     static {
 
     }
     public static void main(String[] args) throws CloneNotSupportedException {
 
-        Animal animal = new Dog();
-        Dog dog = (Dog)animal;
 
+//        String s = "debab";
+//        list=new ArrayList<>();
+//        List<String> running=new ArrayList<>();
+//        visit(0, s, running);
+//        System.out.println(list);
 
+        f("abc", 0);
     }
+
+    public static void f(String s, int index) {
+        if(index == s.length()) {
+            System.out.println(s);
+            return;
+        }
+
+        for(int i=index;i<s.length();i++) {
+            s = swap(s, index, i);
+            f(s, index+1);
+            s = swap(s, index, i);
+        }
+    }
+
+    public static String swap(String s, int i, int j) {
+        char[] c=s.toCharArray();
+        char tmp = c[i];
+        c[i] = c[j];
+        c[j] = tmp;
+        return String.valueOf(c);
+    }
+
+    public static void visit(int i, String s, List<String> running) {
+        list.add(new ArrayList<>(running));
+        String mod = String.join("", running);
+        if(isPalindrom(mod))
+            System.out.println(running);
+
+        for(int j=i;j<s.length();j++) {
+//            if(j>i && s.charAt(j) == s.charAt(j-1))
+//                continue;
+
+            running.add(String.valueOf(s.charAt(j)));
+            visit(j+1, s, running);
+            running.remove(running.size()-1);
+        }
+    }
+
+    public static boolean isPalindrom(String s) {
+        if(s.isEmpty() || s.isBlank())
+            return false;
+
+        StringBuilder s2 = new StringBuilder(s).reverse();
+        return s.equals(s2.toString());
+    }
+
+
+
     public static void call() throws Ex1 {
         throw new Ex1("ex1");
     }
